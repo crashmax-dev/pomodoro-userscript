d715fe1b0df71d99ac56987e273e2ddaa23dbe11
crashmax-dev <crashmax.dev@gmail.com>
2023-03-26 08:18:01 +0000

    Deploying to gh-pages from @ crashmax-dev/pomodoro-userscript@e83e6c43684273f65907a9eca2b558f39b31bd8a 🚀

---
+++ pomodoro-userscript.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        pomodoro-userscript
+// @version     0.0.1
+// @author      crashmax <userscript@crashmax.ru>
+// @license     MIT
+// @homepage    https://crashmax-dev.github.io/pomodoro-userscript/
+// @match       http://localhost:3000
+// @match       https://example.com
+// @grant       GM_addStyle
+// @updateURL   https://crashmax-dev.github.io/pomodoro-userscript/pomodoro-userscript.meta.js
+// @downloadURL https://crashmax-dev.github.io/pomodoro-userscript/pomodoro-userscript.user.js
+// ==/UserScript==
